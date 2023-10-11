1af68d0e2cad676ed217124ae3350fc68b368e0b
wetieF <liwat@hotmail.com>
2023-10-11 10:59:08 +0200

    remove empty line in phase3

--- src/main/java/com/itechprogress/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/itechprogress/cicd/GithubCicdActionsApplication.java
@@ -13,7 +13,6 @@ public class GithubCicdActionsApplication {
 	public String getStudent() {
 		return "Welcome, I am learning github actions.";
 	}
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
