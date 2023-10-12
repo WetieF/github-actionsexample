c4af314adc1e3752fa4c279486fa2f69cb1b8d1b
wetieF <liwat@hotmail.com>
2023-10-12 11:35:12 +0200

    Änderung branch phase3

--- src/main/java/com/itechprogress/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/itechprogress/cicd/GithubCicdActionsApplication.java
@@ -3,13 +3,15 @@ package com.itechprogress.cicd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/api")
 public class GithubCicdActionsApplication {
 
-	@GetMapping("/welcome")
+	@GetMapping("/welcome/students")
 	public String getStudent() {
 		return "Welcome, I am learning github actions.";
 	}
